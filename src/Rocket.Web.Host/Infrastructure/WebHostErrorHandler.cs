e264fbae03711eaa4c753e20a6c2565f9f123acc
Gabriel Palma <gubpalma@hotmail.com>
2026-01-03 13:52:05 +1100

    feat: add reusable components and enhance UI consistency
    Introduced new reusable components like `GoBackButton` and `SimpleTableRowPair` for better code structure and readability. Upgraded `MudBlazor` to version 8.15.0 and implemented adjustments for improved UI consistency, including styling updates and navigation fixes. Additionally, enhanced error handling and integrated a more structured error management mechanism.

--- src/Rocket.Web.Host/Infrastructure/WebHostErrorHandler.cs
+++ src/Rocket.Web.Host/Infrastructure/WebHostErrorHandler.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using MudBlazor;
 using Rocket.Domain.Exceptions;
 
 namespace Rocket.Web.Host.Infrastructure
 {
-    public class WebHostErrorHandler(ISnackbar snackbar)
-        : IWebHostErrorHandler
+    public class WebHostErrorHandler(
+        ISnackbar snackbar,
+        NavigationManager navigationManager
+        ) : IWebHostErrorHandler
     {
         public void HandleException(Exception ex)
         {
             switch (ex)
             {
+                case HttpRequestException hex:
+                    if (hex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        navigationManager
+                            .NavigateTo("/login");
+                    }
+                    break;
                 case RocketException rex:
                     throw rex;
                 case TaskCanceledException:
