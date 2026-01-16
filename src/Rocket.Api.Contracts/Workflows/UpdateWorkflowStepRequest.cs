05c3c76aa186df15c3eff81f587025e62f5b5b9f
Gabriel Palma <gubpalma@hotmail.com>
2026-01-16 16:05:51 +1100

    fix: added missing files

---
+++ src/Rocket.Api.Contracts/Workflows/UpdateWorkflowStepRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Rocket.Api.Contracts.Workflows
+{
+    public class UpdateWorkflowStepRequest<T> where T : WorkflowStepSummary
+    {
+        [JsonPropertyName("workflow_id")]
+        public string WorkflowId { get; set; }
+
+        [JsonPropertyName("workflow_step_id")]
+        public string WorkflowStepId { get; set; }
+
+        [JsonPropertyName("connection_id")]
+        public string ConnectionId { get; set; }
+
+        [JsonPropertyName("step")]
+        public T Step { get; set; }
+    }
+}
