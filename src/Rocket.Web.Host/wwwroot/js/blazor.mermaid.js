b0700257025d06310c06e52595a0be0e0379c854
Gabriel Palma <gubpalma@hotmail.com>
2026-01-19 11:55:50 +1100

    fix: corrected JS loading intermittent issues on workflow display #14

--- src/Rocket.Web.Host/wwwroot/js/blazor.mermaid.js
+++ src/Rocket.Web.Host/wwwroot/js/blazor.mermaid.js
@@ -12,3 +12,29 @@ window.blazorNavigateToRoute = (route) => {
         window.blazorDotNetRef.invokeMethodAsync('NavigateToRoute', route);
     }
 };
+
+window.initializeMermaid = () => {
+    mermaid.initialize({
+        startOnLoad: false,
+        securityLevel: 'loose',
+        theme: 'base',
+        themeVariables: {
+            fontFamily: 'JetBrains Mono, sans-serif',
+            primaryColor: '#1a4d2e',
+            primaryTextColor: '#3a881b',
+            primaryBorderColor: '#3a881b',
+            lineColor: '#3a881b',
+            secondaryColor: '#2d6a4f',
+            tertiaryColor: '#081c15',
+            background: '#081c15',
+            mainBkg: '#e8f9eb',
+            secondBkg: '#2d6a4f',
+            tertiaryBkg: '#52b788',
+            nodeBorder: '#74c69d',
+            clusterBkg: '#1a4d2e',
+            clusterBorder: '#2d6a4f',
+            textColor: '#3a881b',
+            edgeLabelBackground: '#9dcc8e'
+        }
+    });
+};
