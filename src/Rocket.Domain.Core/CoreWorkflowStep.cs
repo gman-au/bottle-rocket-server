47a7f6b496785f0e3085dda243f4ec840696733f
Gabriel Palma <gubpalma@hotmail.com>
2026-01-24 14:34:36 +1100

    fix: added missing file

---
+++ src/Rocket.Domain.Core/CoreWorkflowStep.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Rocket.Domain.Core
+{
+    public abstract record CoreWorkflowStep
+    {
+        public string Id { get; set; }
+
+        public string ConnectorId { get; set; }
+
+        public abstract int[] InputTypes { get; set; }
+
+        public abstract int OutputType { get; set; }
+
+        public abstract string StepName { get; set; }
+
+        public abstract string RequiresConnectorCode { get; set; }
+
+        public IEnumerable<CoreWorkflowStep> ChildSteps { get; set; }
+    }
+}
