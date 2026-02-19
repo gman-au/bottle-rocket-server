0860fad7bb70312cac06819e562c4b45dddb96d4
Gabriel Palma <gubpalma@hotmail.com>
2026-02-20 10:06:39 +1100

    fix: added V1D product type to templates #67

--- src/Rocket.Domain/Enum/RocketbookProductTypeEnum.cs
+++ src/Rocket.Domain/Enum/RocketbookProductTypeEnum.cs
@@ -3,7 +3,8 @@
     public enum RocketbookProductTypeEnum
     {
         FusionV16 = 0x16,
-        Fusion = 0x1F,
-        Core = 0x17
+        FusionV1F = 0x1F,
+        CoreV17 = 0x17,
+        CoreV1D = 0x1D
     }
 }
