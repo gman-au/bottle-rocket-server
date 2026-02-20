077590fd27c27914fd661cb2000191e644d24365
Gabriel Palma <gubpalma@hotmail.com>
2026-02-20 13:41:30 +1100

    feat: add API versioning endpoint and integrate version display #70
    Introduced a new `/api/version` endpoint in `VersionController` to provide API versioning details. Updated the front-end to display both API and web version details retrieved via `ApiRequestManager`. Enhanced Dockerfiles to support dynamic versioning during build.

---
+++ src/Rocket.Api.Host/Controllers/VersionController.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Rocket.Api.Contracts;
+using Rocket.Api.Host.Extensions;
+
+namespace Rocket.Api.Host.Controllers
+{
+    [ApiController]
+    [Route("/api/version")]
+    public class VersionController(ILogger<VersionController> logger) : ControllerBase
+    {
+        [HttpGet]
+        [EndpointSummary("Version")]
+        [EndpointGroupName("Status")]
+        [EndpointDescription("Returns the current version server API.")]
+        [ProducesResponseType(
+            typeof(VersionResponse),
+            StatusCodes.Status200OK
+        )]
+        public IActionResult GetHealthCheck()
+        {
+            logger
+                .LogInformation("Received version request");
+
+            var apiVersion =
+                Assembly
+                    .GetExecutingAssembly()
+                    .GetName()
+                    .Version
+                    ?.ToString()
+                ?? "dev";
+
+            var response = new VersionResponse
+            {
+                ApiVersion = apiVersion,
+                WebVersion = null
+            };
+
+            return
+                response
+                    .AsApiSuccess();
+        }
+    }
+}
